f62b12f94387436e77e4f04ccd1cd37b85ebfb74
Nikhil Nagar <nikhilnagarcps@gmail.com>
2024-05-23 23:24:46 +0530

    galaxy generator done

--- haunted-house/main.js
+++ haunted-house/main.js
@@ -19,32 +19,52 @@ const scene = new THREE.Scene();
  */
 const textureLoader = new THREE.TextureLoader();
 
-const doorColorTexture = textureLoader.load("/textures/door/color.jpg");
-const doorAlphaTexture = textureLoader.load("/textures/door/alpha.jpg");
+const doorColorTexture = textureLoader.load(
+  "/haunted-house/textures/door/color.jpg"
+);
+const doorAlphaTexture = textureLoader.load(
+  "/haunted-house/textures/door/alpha.jpg"
+);
 const doorAmbientOcclusionTexture = textureLoader.load(
-  "/textures/door/ambientOcclusion.jpg"
+  "/haunted-house/textures/door/ambientOcclusion.jpg"
 );
-const doorHeightTexture = textureLoader.load("/textures/door/height.jpg");
-const doorNormalTexture = textureLoader.load("/textures/door/normal.jpg");
-const doorMetalnessTexture = textureLoader.load("/textures/door/metalness.jpg");
-const doorRoughnessTexture = textureLoader.load("/textures/door/roughness.jpg");
+const doorHeightTexture = textureLoader.load(
+  "/haunted-house/textures/door/height.jpg"
+);
+const doorNormalTexture = textureLoader.load(
+  "/haunted-house/textures/door/normal.jpg"
+);
+const doorMetalnessTexture = textureLoader.load(
+  "/haunted-house/textures/door/metalness.jpg"
+);
+const doorRoughnessTexture = textureLoader.load(
+  "/haunted-house/textures/door/roughness.jpg"
+);
 
-const bricksColorTexture = textureLoader.load("/textures/bricks/color.jpg");
+const bricksColorTexture = textureLoader.load(
+  "/haunted-house/textures/bricks/color.jpg"
+);
 const bricksAmbientOcclusionTexture = textureLoader.load(
-  "/textures/bricks/ambientOcclusion.jpg"
+  "/haunted-house/textures/bricks/ambientOcclusion.jpg"
 );
-const bricksNormalTexture = textureLoader.load("/textures/bricks/normal.jpg");
+const bricksNormalTexture = textureLoader.load(
+  "/haunted-house/textures/bricks/normal.jpg"
+);
 const bricksRoughnessTexture = textureLoader.load(
-  "/textures/bricks/roughness.jpg"
+  "/haunted-house/textures/bricks/roughness.jpg"
 );
 
-const grassColorTexture = textureLoader.load("/textures/grass/color.jpg");
-const grassAmbientOcclusionTexture = textureLoader.load(
-  "/textures/grass/ambientOcclusion.jpg"
+const grassColorTexture = textureLoader.load(
+  "/haunted-house/textures/grass/color.jpg"
 );
-const grassNormalTexture = textureLoader.load("/textures/grass/normal.jpg");
+const grassAmbientOcclusionTexture = textureLoader.load(
+  "/haunted-house/textures/grass/ambientOcclusion.jpg"
+);
+const grassNormalTexture = textureLoader.load(
+  "/haunted-house/textures/grass/normal.jpg"
+);
 const grassRoughnessTexture = textureLoader.load(
-  "/textures/grass/roughness.jpg"
+  "/haunted-house/textures/grass/roughness.jpg"
 );
 grassColorTexture.repeat.set(8, 8);
 grassAmbientOcclusionTexture.repeat.set(8, 8);
